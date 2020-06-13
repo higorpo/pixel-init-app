a856af5422f618862a0de332153b685ae9449ac3
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-06-13 12:22:13 -0300

    :wrench: Configure babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-nullish-coalescing-operator'
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      [
+        'babel-plugin-root-import',
+        {
+          "paths": [
+            {
+              "rootPathSuffix": "./src",
+              "rootPathPrefix": "~/"
+            },
+            {
+              "rootPathSuffix": "./assets",
+              "rootPathPrefix": "assets/"
+            },
+          ]
+        }
+      ]
     ]
   };
 };
