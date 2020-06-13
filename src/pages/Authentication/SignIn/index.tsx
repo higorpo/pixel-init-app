8d0cd132e78d5074deccaa5ec41d76931a4f09f2
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-06-13 12:46:46 -0300

    :sparkles: Create sign-in screen

--- src/pages/Authentication/SignIn/index.tsx
+++ src/pages/Authentication/SignIn/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Dimensions, StatusBar, Platform } from 'react-native';
+import { Button } from '../../../components';
+import { Container, ImageContainer, ImageLogo, Title } from './styles';
 
 const SignIn: React.FC = () => {
-    return <View />;
+    return (
+        <Container>
+            {Platform.OS == "android" && <StatusBar translucent={true} backgroundColor="transparent" />}
+            <ImageContainer
+                resizeMethod="scale"
+                resizeMode="cover"
+            >
+                <ImageLogo />
+            </ImageContainer>
+            <Title>
+                Entre no aplicativo para ter acesso as informações do evento.
+            </Title>
+            <Button
+                label="entrar"
+            />
+            <Button
+                label="registrar-se"
+                color="secondary"
+                style={{
+                    marginTop: 10
+                }}
+            />
+        </Container>
+    );
 }
 
 export default SignIn;
