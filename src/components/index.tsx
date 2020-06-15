4145bf61f8854f824913adfaec9c7223bc9488c2
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-06-15 12:45:39 -0300

    :hammer: Moves the container component to the global components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
+export { Container } from "./styles";
 export { default as Button } from "./Button";
 export { default as BackButtonNavigator } from "./BackButtonNavigator";
 export { default as TextInput } from "./TextInput";
